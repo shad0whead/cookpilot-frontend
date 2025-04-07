ba9f68dc00083bc025eb5fd216068717edafebaa
shad0whead <mfcoxjr@gmail.com>
2025-04-07 00:28:08 -0400

    Add files via upload

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
